7914cac132a660d33c0a8b88aa77fc4e0d701226
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2021-08-26 14:15:27 +0200

    DE for multi-d time series

--- main.py
+++ main.py
@@ -3,7 +3,51 @@ import numpy as np
 
 def delay_embed(x, k, tau=1, typ='asy'):
     """
-    Delay-embedking for scalar time-series x(t).
+    Delay-embedding for multi-dimensional time series x(t)
+
+    Parameters
+    ----------
+    x : Txd array
+        Multi-dimensional time series.
+    k : int
+        Embedding dimension. Needs to be at most 2*d+1, possibly smaller..
+    tau : int, optional
+        Delay parameter. The default is 1.
+    typ : TYPE, optional
+        DESCRIPTION. The default is 'asy'.
+
+    Returns
+    -------
+    Yd : (T-(k-1)*tau)*kd numpy array
+        Time asymmetric or symmetric embedking. The default is 'asy'..
+
+    """
+    
+    
+    if len(x.shape)==1:
+        x = x[:,None]
+        
+    dim = x.shape[1]
+    
+    #delay embed all dimensions
+    Y = []
+    for d in range(dim):
+        Ytmp = delay_embed_scalar(x[:,d], k, tau=1, typ='asy')
+        Y.append(Ytmp)
+    
+    #interleave dimensions
+    shape = (Y[0].shape[0], Y[0].shape[1]*len(Y))
+    # Y = np.vstack(Y).reshape(shape)
+    Yd = np.empty(shape)
+    for i in range(dim):
+        Yd[:,i::dim]=Y[i]
+    
+    return Yd
+
+
+def delay_embed_scalar(x, k, tau=1, typ='asy'):
+    """
+    Delay-embedding for scalar time-series x(t).
     Builds prekictive coorkinates Y=[x(t), x(t-tau), ..., x(t-(k-1)*tau)]
 
     Parameters
@@ -11,7 +55,7 @@ def delay_embed(x, k, tau=1, typ='asy'):
     x : Tx1 array
         Scalar time series.
     k : int
-        Embedking kension. Needs to be at most 2*d+1, possibly smaller.
+        Embedding dimension. Needs to be at most 2*d+1, possibly smaller.
     tau : int, optional
         Delay parameter. The default is 1.
     typ : 'asy', 'sym', optional
@@ -19,13 +63,13 @@ def delay_embed(x, k, tau=1, typ='asy'):
 
     Returns
     -------
-    Y : kx(T-(k-1)*tau) numpy array
+    Y : (T-(k-1)*tau)*k numpy array
         Delay embedded coorkinates.
 
     """
     
-    T = len(x)
-    N = T - (k-1)*tau # length of embedded signal  
+    T = x.shape[0] #length of time series
+    N = T - (k-1)*tau # length of embedded signal
     Y = np.zeros([N,k])
         
     if typ == 'asy':  
@@ -42,45 +86,5 @@ def delay_embed(x, k, tau=1, typ='asy'):
         for ki in range(-(k-1)//2,(k-1)//2+1):
             ind = np.arange(((k-1)//2+ki)*tau, N+((k-1)//2+ki)*tau)
             Y[:,ki] = x[ind]
-          
+            
     return Y
-
-
-# def delay_embed_multid(Y,k,tau,typ='asy'):
-    
-#     if len(Y.shape) == 1:
-#         return delay_embed(Y,k,tau,typ)
-        
-#     dtaumax = np.max((k-1)*tau)
-#     nrow = Y.shape[0] - dtaumax # length of embedded signal
-#     D = Y.shape[1] # number of variables
-    
-#     if type(tau) is int:
-#         tau = np.repeat(tau,D)
-        
-#     if type(k) is int:
-#         k = np.repeat(k,D)
-        
-#     X = np.zeros([nrow,np.sum(k)])
-        
-#     assert (len(tau) == D) & (len(k) == D)
-    
-#     if typ == 'asy':
-#         for i in range(D):
-#             for ki in range(k[i]):
-#                 ind = np.sum(k[:i]) + ki
-#                 X[:,ind] = Y[ np.arange(ki*tau[i], nrow+ki*tau[i]), i ]
-            
-#     elif typ == 'sym' and k[i] % 2 == 0:
-#         for i in range(D):
-#             for ki in range(-k[i]//2,k[i]//2):
-#                 ind = np.sum(k[:i]) + k[i]//2 + ki 
-#                 X[:,ind] = Y[ np.arange((k[i]//2+ki)*tau[i], nrow+(k[i]//2+ki)*tau[i]), i ]
-            
-#     elif typ == 'sym' and k[i] % 2 == 1:
-#         for i in range(D):
-#             for ki in range(-(k[i]-1)//2,(k[i]-1)//2+1):
-#                 ind = np.sum(k[:i]) + (k[i]-1)//2 + ki
-#                 X[:,ind] = Y[ np.arange(((k[i]-1)//2+ki)*tau[i], nrow+((k[i]-1)//2+ki)*tau[i]), i ]
-    
-#     return X
