413af7136fcf313f25346a3fd3f54cfbc9c12309
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2022-05-13 13:04:19 +0200

    simplify

--- GeoDySys/utils.py
+++ GeoDySys/utils.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
 import torch
-
 import networkx as nx
+
 from torch_geometric.utils.convert import to_networkx
 from torch_geometric.nn import knn_graph
 from torch_geometric.utils import to_undirected
 from torch_geometric.data import Data
 from torch_geometric.data.collate import collate
+from torch_geometric.transforms import RandomNodeSplit
+
 from cknn import cknneighbors_graph
-from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
-from sklearn.metrics import pairwise_distances
 
 
 def construct_data_object(x, y, graph_type='knn', k=10):
@@ -33,57 +32,20 @@ def construct_data_object(x, y, graph_type='knn', k=10):
         data_.num_nodes = len(x[i])
         data_.num_node_features = data_.x.shape[1]
         data_.y = torch.ones(data_.num_nodes, dtype=int)*i
-        data_ = split(data_, test_size=0.1, val_size=0.5, seed=0)
         
         G.append(G_)
         data_list.append(data_)
         
     #collate datasets
-    data_train, slices, _ = collate(data_list[0].__class__,
-                                    data_list=data_list,
-                                    increment=True,
-                                    add_batch=False)
+    data, slices, _ = collate(data_list[0].__class__,
+                              data_list=data_list,
+                              increment=True,
+                              add_batch=False)
     
-    return data_train, slices, G
+    split = RandomNodeSplit(split='train_rest', num_val=0.1, num_test=0.1)
+    split(data)
     
-    
-def split(data, test_size=0.1, val_size=0.5, seed=0):
-    """
-    Split training and test datasets
-
-    Parameters
-    ----------
-    data : pytorch geometric data object. All entried must be torch tensors
-            for this to work properly.
-    test_size : float between 0 and 1, optional
-        Test set as fraction of total dataset. The default is 0.1.
-    val_size : float between 0 and 1, optional
-        Validation set as fraction of test set. The default is 0.5.
-    seed : int, optional
-        Seed. The default is 0.
-
-    Returns
-    -------
-    data : pytorch geometric data object.
-
-    """
-    
-    n = len(data.x)
-    train_id, test_id = train_test_split(np.arange(n), test_size=test_size, random_state=seed)
-    test_id, val_id = train_test_split(test_id, test_size=val_size, random_state=seed)
-    
-    train_mask = torch.zeros(n, dtype=bool)
-    test_mask = torch.zeros(n, dtype=bool)
-    val_mask = torch.zeros(n, dtype=bool)
-    train_mask[train_id] = True
-    test_mask[test_id] = True
-    val_mask[val_id] = True
-    
-    data.train_mask = train_mask
-    data.test_mask = test_mask
-    data.val_mask = val_mask
-    
-    return data
+    return data, slices, G
 
 
 def fit_knn_graph(X, k=10, graph_type='cknn'):
