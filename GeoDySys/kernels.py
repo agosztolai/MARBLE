bug fix directional average filter
forgot to take absolute values

GeoDySys/kernels.py
@@ -56,8 +56,7 @@ def project_gauge_to_neighbours(data, gauge='global'):
 
 def DD(data, gauge):
     """
-    This function implements the directional derivative kernel 
-    from Beaini et al. 2021.
+    Directional derivative kernel from Beaini et al. 2021.
 
     Parameters
     ----------
@@ -84,8 +83,7 @@ def DD(data, gauge):
 
 def DA(data, gauge):
     """
-    This function implements the directional average kernel 
-    from Beaini et al. 2021.
+    Directional average kernel from Beaini et al. 2021.
 
     Parameters
     ----------
@@ -105,6 +103,6 @@ def DA(data, gauge):
     K = []
     for _F in F:
         Fhat = normalize(_F, dim=-1, p=1)
-        K.append(Fhat)
+        K.append(torch.abs(Fhat))
         
     return K

test/kernel_test.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from GeoDySys.kernels import DD
+from GeoDySys.kernels import DD, DA
 from GeoDySys import utils
 import matplotlib.pyplot as plt
 
@@ -52,15 +52,12 @@ for ind in range(x.shape[0]):
     
 PCM=ax1.get_children()[0] #get the mappable, the 1st and the 2nd are the x and y axes
 plt.colorbar(PCM, ax=ax1)
-plt.savefig('../results/kernel_linear.svg')
+# plt.savefig('../results/kernel_linear.svg')
 
 #parabola
 def f2(x, alpha):
     return np.cos(alpha)*x[:,[0]]**2# - np.sin(alpha)*x[:,[1]]**2
 y = f2(x, alpha)
-
-data = utils.construct_dataset(x, y, graph_type='cknn', k=k)
-K = DD(data, 'global')
 
 der = np.hstack([np.matmul(K[0],y),np.matmul(K[1],y)])
 derder = np.hstack([np.matmul(K[0],der[:,[0]]),np.matmul(K[1],der[:,[0]]),np.matmul(K[0],der[:,[1]]),np.matmul(K[1],der[:,[1]])])
@@ -89,6 +86,30 @@ for ind in range(x.shape[0]):
     ax3.arrow(x[ind,0], x[ind,1], derder[ind,1], 0, width=0.01, color='r')
     ax3.arrow(x[ind,0], x[ind,1], 0, derder[ind,2], width=0.01, color='b')
     
-# PCM=ax1.get_children()[0] #get the mappable, the 1st and the 2nd are the x and y axes
-# plt.colorbar(PCM, ax=ax1)
-plt.savefig('../results/kernel_parabola.svg')
+PCM=ax1.get_children()[0] #get the mappable, the 1st and the 2nd are the x and y axes
+plt.colorbar(PCM, ax=ax1)
+# plt.savefig('../results/kernel_parabola.svg')
+
+#linear directional average
+def f1(x, alpha):
+    return np.cos(alpha)*x[:,[0]] + np.sin(alpha)*x[:,[1]]
+y = f1(x, alpha)
+
+data = utils.construct_dataset(x, y, graph_type='cknn', k=k)
+K = DA(data, 'global')
+
+av = np.hstack([np.matmul(K[0],y),np.matmul(K[1],y)])
+
+f, (ax1) = plt.subplots(1, 1, sharey=True, figsize=(4, 3),
+                                subplot_kw={'aspect': 1})
+ax1.scatter(x[:,0], x[:,1], c=y)
+ax1.set_title(r'$(f_x,f_y)$')
+ax1.axis('off')
+
+for ind in range(x.shape[0]):
+    ax1.arrow(x[ind,0], x[ind,1], av[ind,0], av[ind,1], width=0.01)
+
+    
+PCM=ax1.get_children()[0] #get the mappable, the 1st and the 2nd are the x and y axes
+plt.colorbar(PCM, ax=ax1)
+# plt.savefig('../results/kernel_linear.svg')