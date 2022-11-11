50d79987dc75ba2bd55d401276193d806ea88c54
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2022-11-11 16:56:46 +0100

    Update __init__.py

--- MARBLE/__init__.py
+++ MARBLE/__init__.py
@@ -1,1 +1,1 @@
-from .preprocessing import preprocessingfrom .postprocessing import postprocessingfrom .main import netfrom .lib import *
+from .preprocessing import preprocessingfrom .postprocessing import postprocessing, compare_attractorsfrom .main import netfrom .lib import *
