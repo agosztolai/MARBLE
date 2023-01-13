bbba29572faa81b7e72a0b3a8031dad410bfe7c4
agosztolai <a.gosztolai@gmail.com>
2023-01-13 13:11:08 +0100

    example

--- examples/example_utils.py
+++ examples/example_utils.py
@@ -126,3 +126,37 @@ def plot_phase_portrait(pos, vel, ax=None, node_feature=None, style='>', lw=2, s
         ax = plotting.trajectories(p, v, ax=ax, style=style, node_feature=nf, lw=lw, scale=scale, axis=False, alpha=1., arrow_spacing=spacing)
         
     return ax
+
+
+def spiking_data(file = '../data/conditions_spiking_data.mat'):
+    
+    #data is a matrix with shape (trials, conditions)
+    data = loadmat(file)['result']
+    
+    conditions=['DownLeft','DownRight','Left','Right','UpLeft','UpRight','Up']
+    
+    rates = {}
+    for i, cond in enumerate(conditions):
+                
+        rates_trial = []
+        for t, trial in enumerate(data[:,i]):
+            if trial.shape[1] != 0:                
+                try:
+                    rates_channel = []
+                    #trial[0][0] is a matrix with shape (channel, timesteps)
+                    for c, channel in enumerate(trial[0][0]):
+                        spikes = np.where(channel)[0]
+                        
+                        st = neo.SpikeTrain(spikes, units='ms', t_stop=1200)
+                        r = instantaneous_rate(st, sampling_period=50*ms).magnitude
+
+                        rates_channel.append(r.flatten())
+                                                                    
+                    rates_trial.append(np.vstack(rates_channel))
+                except:
+                    continue
+            
+        #rates[cond] is a matrix with shape (trials, channels, timesteps)
+        rates[cond] = np.stack(rates_trial, axis=0)
+        
+    pickle.dump(rates, open('../data/rate_data.pkl','wb'))
