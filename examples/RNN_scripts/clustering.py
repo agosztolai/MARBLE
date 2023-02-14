a6e48406eea6540e9b685722c9df25221d23f24d
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2023-02-14 11:59:34 +0100

    Update clustering.py

--- examples/RNN_scripts/clustering.py
+++ examples/RNN_scripts/clustering.py
@@ -135,14 +135,14 @@ def to_support_net(net, z, new_size=None, take_means=False, scaling=False):
     wi_init = torch.zeros(net.input_size, n_components, basis_dim)
     wo_init = torch.zeros(net.output_size, n_components, basis_dim)
 
-    if new_size is None:
-        new_size = net.hidden_size
-    if scaling:
-        old_size = net.hidden_size
-    else:
-        old_size = 1
-    m_means = torch.from_numpy(means[:, :rank]).t() * sqrt(old_size) / sqrt(new_size)
-    n_means = torch.from_numpy(means[:, rank: 2*rank]).t() * sqrt(old_size) / sqrt(new_size)
+    # if new_size is None:
+    #     new_size = net.hidden_size
+    # if scaling:
+    #     old_size = net.hidden_size
+    # else:
+    #     old_size = 1
+    m_means = torch.from_numpy(means[:, :rank]).t() #* sqrt(old_size) / sqrt(new_size)
+    n_means = torch.from_numpy(means[:, rank: 2*rank]).t() #* sqrt(old_size) / sqrt(new_size)
     wi_means = torch.from_numpy(means[:, 2*rank: 2*rank + net.input_size]).t()
 
     for i in range(n_components):
@@ -150,12 +150,12 @@ def to_support_net(net, z, new_size=None, take_means=False, scaling=False):
         G = covariances[i]
         X_reduced = gram_factorization(G)
         for k in range(rank):
-            m_init[k, i] = torch.from_numpy(X_reduced[k]) * sqrt(old_size) / sqrt(new_size)
-            n_init[k, i] = torch.from_numpy(X_reduced[rank + k]) * sqrt(old_size) / sqrt(new_size)
+            m_init[k, i] = torch.from_numpy(X_reduced[k]) #* sqrt(old_size) / sqrt(new_size)
+            n_init[k, i] = torch.from_numpy(X_reduced[rank + k]) #* sqrt(old_size) / sqrt(new_size)
         for k in range(net.input_size):
             wi_init[k, i] = torch.from_numpy(X_reduced[2 * rank + k])
         for k in range(net.output_size):
-            wo_init[k, i] = torch.from_numpy(X_reduced[2 * rank + net.input_size + k]) * old_size / new_size
+            wo_init[k, i] = torch.from_numpy(X_reduced[2 * rank + net.input_size + k]) #* old_size / new_size
 
     net2 = SupportLowRankRNN(net.input_size, new_size, net.output_size, net.noise_std, net.alpha, rank, n_components,
                              weights, basis_dim, m_init, n_init, wi_init, wo_init, m_means, n_means, wi_means)
