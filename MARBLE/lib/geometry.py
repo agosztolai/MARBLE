34662fba8c2cde8da5ba5cb4930ad9f81ec693d8
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2023-02-15 13:34:30 +0100

    Update geometry.py

--- MARBLE/lib/geometry.py
+++ MARBLE/lib/geometry.py
@@ -340,7 +340,9 @@ def gradient_op(pos, edge_index, gauges):
     
     """
     
+    print('neighbours')
     nvec = neighbour_vectors(pos, edge_index, normalise=False) #(nxnxdim)
+    print('project gauges')
     F = project_gauge_to_neighbours(nvec, gauges)
     
     K = []
