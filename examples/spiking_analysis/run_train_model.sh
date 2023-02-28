66d9783c133a0a28ad96a8e021206f21eb509d73
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2023-02-28 08:18:51 +0100

    Update run_train_model.sh

--- examples/spiking_analysis/run_train_model.sh
+++ examples/spiking_analysis/run_train_model.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name spiking_to_rates # Name for your job
 #SBATCH --ntasks 1              # Number of (cpu) tasks
 #SBATCH --time 120             # Runtime in minutes.
-#SBATCH --mem 50000             # Reserve 10 GB RAM for the job
+#SBATCH --mem 19000             # Reserve 10 GB RAM for the job
 #SBATCH --partition cpu         # Partition to submit ('gpu' or 'cpu')
 #SBATCH --qos staff             # QOS ('staff' or 'students')
 #SBATCH --gres gpu:gtx1080:1
