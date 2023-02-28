7189f05f93b82758f24529ad32f013eba4066e96
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2023-02-28 08:17:49 +0100

    .

---
+++ examples/spiking_analysis/run_train_model.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name spiking_to_rates # Name for your job
+#SBATCH --ntasks 1              # Number of (cpu) tasks
+#SBATCH --time 120             # Runtime in minutes.
+#SBATCH --mem 50000             # Reserve 10 GB RAM for the job
+#SBATCH --partition cpu         # Partition to submit ('gpu' or 'cpu')
+#SBATCH --qos staff             # QOS ('staff' or 'students')
+#SBATCH --gres gpu:gtx1080:1
+#SBATCH --output myjob-%j.txt       # Standard out goes to this file
+#SBATCH --error myjob-%j.txt        # Standard err goes to this file
+#SBATCH --mail-user adam.gosztolai@epfl.ch
+#SBATCH --mail-type ALL         # ALL will alert you of job beginning, completion, failure etc
+           # Reserve 1 GPU for usage, can be 'teslak40', 'gtx1080', or 'titanrtx'
+#SBATCH --chdir . 
+
+# ACTIVATE ANACONDA
+eval "$(conda shell.bash hook)"
+conda activate MARBLE
+
+# RUN TRAINING
+python train_model.py
---
+++ examples/spiking_analysis/train_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pickle
+from MARBLE import net
+
+data, days, conditions = pickle.load(open('../outputs/spiking_data/data_dataobject_k20.pkl','rb'))
+
+par = {'epochs': 100, #optimisation epochs
+       'order': 2, #order of derivatives
+       'hidden_channels': 64, #number of internal dimensions in MLP
+       'out_channels': 5,
+       'inner_product_features': False,
+       'vec_norm': False,
+       'diffusion': False,
+       }
+    
+model = net(data, **par)
+    
+model.run_training(data, use_best=True, outdir='../outputs/spiking_data/k20') 
