34d95655e1980d91db860cbad627133a0385f77a
peach-lucien <robertlucienpeach@gmail.com>
2023-03-02 17:19:54 +0100

    analysis notebooks for spikling plots

--- examples/spiking_analysis/per_session_analysis_20ms/per_session_pca_analysis_20ms.py
+++ examples/spiking_analysis/per_session_analysis_20ms/per_session_pca_analysis_20ms.py
@@ -30,7 +30,8 @@ def main():
     """ fitting model for each day + pca embedding """    
     
     # instantaneous rate data
-    rates =  pickle.load(open('../rate_data_20ms.pkl','rb'))       
+    rates =  pickle.load(open('../data/rate_data_20ms.pkl','rb'))       
+    trial_ids =  pickle.load(open('../data/trial_ids.pkl','rb'))       
 
     # definingf the set of conditions     
     conditions=['DownLeft','Left','UpLeft','Up','UpRight','Right','DownRight']    
@@ -40,18 +41,17 @@ def main():
     
     # define some parameters
     pca_n = 5
-    rm_outliers = True
+    rm_outliers = False
     filter_data = True
     plot = False
       
     # storing all distance matrices
     embeddings = []
     distance_matrices = []
-    
-    train_embeddings = []
-    train_distance_matrices = []
-    times = [];
-    all_condition_labels = [];
+    times = []; # to store the time point of each node in the trajectory
+    all_condition_labels = []; # to store the condition label for each node
+    all_trial_ids = []; # trial ids for each node
+    all_sampled_ids = []; # to store all the nodes sampled by marble 
     
     # loop over each day
     for day in days:
@@ -90,7 +90,8 @@ def main():
         pos = [[] for u in range(len(conditions))]
         vel = [[] for u in range(len(conditions))]             
         timepoints = [[] for u in range(len(conditions))]    
-        condition_labels = [[] for u in range(len(conditions))]    
+        condition_labels = [[] for u in range(len(conditions))]  
+        trial_indexes = [[] for u in range(len(conditions))]
         
         # loop over conditions
         for c, cond in enumerate(conditions):
@@ -98,7 +99,7 @@ def main():
             # go cue at 500ms (500ms / 50ms bin = 10)
             # only take rates from bin 10 onwards
             data = rates[day][cond][:,:,25:]           
-                       
+            
             # loop over all trials
             for t in range(data.shape[0]):
                 
@@ -120,6 +121,10 @@ def main():
                 
                 timepoints[c].append(np.linspace(0,trial.shape[0]-2,trial.shape[0]-1))
                 condition_labels[c].append(np.repeat(c,trial.shape[0]-1))
+                
+                # adding trial id info (to match with kinematics decoding later)
+                trial_indexes[c].append(np.repeat(trial_ids[day][cond][t],trial.shape[0]-1))
+                
    
         # plt.quiver(pos[0][1][:,0],pos[0][1][:,1],vel[0][1][:,0],vel[0][1][:,1], angles='xy')
         
@@ -128,25 +133,26 @@ def main():
         vel = [np.vstack(u) for u in vel] # trials x time x channels
         timepoints = [np.hstack(u) for u in timepoints] 
         condition_labels = [np.hstack(u) for u in condition_labels]   
-          
+        trial_indexes = [np.hstack(u) for u in trial_indexes]   
+
         # remove outliers
         if rm_outliers:
-            pos, vel, timepoints, condition_labels = remove_outliers(pos, vel, timepoints, condition_labels)
+            pos, vel, timepoints, condition_labels, trial_indexes = remove_outliers(pos, vel, timepoints, condition_labels, trial_indexes)
 
         # construct data for marble
-        data = utils.construct_dataset(pos, features=vel, graph_type='cknn', k=30, stop_crit=0.02, delta=2.0,
+        data = utils.construct_dataset(pos, features=vel, graph_type='cknn', k=30, stop_crit=0.0, delta=2.0,
                                        n_nodes=None, n_workers=1, n_geodesic_nb=10, compute_laplacian=True, vector=False)
         
         
         os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-        os.environ["CUDA_VISIBLE_DEVICES"]="1,2"
+        os.environ["CUDA_VISIBLE_DEVICES"]="0"
         
         #x = data.x[:100,:].numpy()
         #pos = data.pos[:100,:].numpy()
         #plt.quiver(pos[:,0],pos[:,1],x[:,0],x[:,1], angles='xy')
 
         
-        par = {'epochs': 100, #optimisation epochs
+        par = {'epochs': 5, #optimisation epochs
                'order': 2, #order of derivatives
                'hidden_channels': 100, #number of internal dimensions in MLP
                'out_channels': 3,
@@ -159,68 +165,28 @@ def main():
         
         model.run_training(data, use_best=True)        
         data = model.evaluate(data)   
-        
+
+ 
         n_clusters = 50        
         data = postprocessing(data, n_clusters=n_clusters)       
         
-        # extracting the time points associated with data
-        for c,cond in enumerate(conditions):
-            time = timepoints[c]
-            time = time[data.sample_ind[data.y==c]]
-            timepoints[c] = time
 
         embeddings.append(data.out)
         distance_matrices.append(data.dist)
-        times.append(timepoints)
+        times.append(np.hstack(timepoints))
         all_condition_labels.append(data.y)
+        all_trial_ids.append(np.hstack(trial_indexes))
+        all_sampled_ids.append(data.sample_ind)
 
         
         with open('./outputs/distance_matrices_and_embeddings_20ms_sc02.pkl', 'wb') as handle:
-            pickle.dump([distance_matrices, embeddings, times , all_condition_labels, train_distance_matrices, train_embeddings ], handle, protocol=pickle.HIGHEST_PROTOCOL)
+            pickle.dump([distance_matrices, embeddings, times , all_condition_labels, all_trial_ids, all_sampled_ids], handle, protocol=pickle.HIGHEST_PROTOCOL)
             
-    if plot:
-        emb_MDS, _ = geometry.embed(np.dstack(distance_matrices).mean(2), embed_typ = 'MDS')
-        plt.figure()
-        plt.scatter(emb_MDS[:,0],emb_MDS[:,1],c=np.linspace(0,6,7))
-        plt.savefig('./outputs/mds_embedding.png')
-
-        plt.figure()
-        plt.imshow(np.dstack(distance_matrices).mean(2)); plt.colorbar()  
-        plt.savefig('./outputs/distance_matrix.png')
-
-
-
-                    
-    
+            
     
     return
 
-def analyse():
-    
-    with open('./outputs/distance_matrices.pkl', 'rb') as handle:
-        data = pickle.load(handle)
 
-    distance_matrices = data[0]
-    embeddings = data[1]
-    train_distance_matrices = data[2]
-    train_embeddings = data[3]
-    
-    # plot average distance matrix
-    plt.figure()
-    plt.imshow(np.median(np.dstack(train_distance_matrices),2)); plt.colorbar()  
-    
-    # plot average distance matrix
-    emb_MDS, _ = geometry.embed(np.median(np.dstack(distance_matrices),2), embed_typ = 'MDS')
-    plt.figure()
-    plt.scatter(emb_MDS[:,0],emb_MDS[:,1],c=np.linspace(0,6,7))
-    
-    emb_MDS, _ = geometry.embed(np.median(np.dstack(train_distance_matrices),2), embed_typ = 'MDS')
-    plt.figure()
-    plt.scatter(emb_MDS[:,0],emb_MDS[:,1],c=np.linspace(0,6,7))
-    
-    
-
-    #plt.savefig('./outputs/distance_matrix.png')
 
 
 def get_vector_array(coords):
@@ -228,7 +194,7 @@ def get_vector_array(coords):
     diff = np.diff(coords, axis=0)
     return diff
 
-def remove_outliers(pos, vel, timepoints, condition_labels):
+def remove_outliers(pos, vel, timepoints, condition_labels, trial_indexes):
     """  function for removing outliers """
     clf = LocalOutlierFactor(n_neighbors=10)        
     # remove positional outliers
@@ -238,7 +204,8 @@ def remove_outliers(pos, vel, timepoints, condition_labels):
         pos[i] = pos[i][outliers==1]  
         timepoints[i] = timepoints[i][outliers==1]
         condition_labels[i] = condition_labels[i][outliers==1]  
-                         
+        trial_indexes[i] = trial_indexes[i][outliers==1]  
+        
     # remove velocity outliers
     for i,v in enumerate(vel):
         outliers = clf.fit_predict(v)
@@ -246,8 +213,9 @@ def remove_outliers(pos, vel, timepoints, condition_labels):
         pos[i] = pos[i][outliers==1]  
         timepoints[i] = timepoints[i][outliers==1]
         condition_labels[i] = condition_labels[i][outliers==1]  
-        
-    return pos, vel, timepoints, condition_labels
+        trial_indexes[i] = trial_indexes[i][outliers==1]  
+
+    return pos, vel, timepoints, condition_labels, trial_indexes
 
 if __name__ == '__main__':
     sys.exit(main())
