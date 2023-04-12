f71c9a506fc8f5bee3293b4312b65be1d15719dd
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2023-04-12 14:10:13 +0200

    Update postprocessing.py

--- MARBLE/postprocessing.py
+++ MARBLE/postprocessing.py
@@ -88,55 +88,55 @@ def embed_in_2D(data, embed_typ="umap", manifold=None, seed=0):
         
 
 
-def compare_attractors(data, source_target):
-    """Compare attractors."""
-    assert all(
-        hasattr(data, attr) for attr in ["emb", "gamma", "clusters", "cdist"]
-    ), "It looks like postprocessing has not been run..."
+# def compare_attractors(data, source_target):
+#     """Compare attractors."""
+#     assert all(
+#         hasattr(data, attr) for attr in ["emb", "gamma", "clusters", "cdist"]
+#     ), "It looks like postprocessing has not been run..."
 
-    s, t = source_target
-    slices = data._slice_dict["x"]  # pylint: disable=protected-access
-    n_slices = len(slices) - 1
-    s_s = range(slices[s], slices[s + 1])
-    s_t = range(slices[t], slices[t + 1])
+#     s, t = source_target
+#     slices = data._slice_dict["x"]  # pylint: disable=protected-access
+#     n_slices = len(slices) - 1
+#     s_s = range(slices[s], slices[s + 1])
+#     s_t = range(slices[t], slices[t + 1])
 
-    assert s < n_slices - 2 and t < n_slices - 1, "Source and target must be < number of slices!"
-    assert s != t, "Source and target must be different!"
+#     assert s < n_slices - 2 and t < n_slices - 1, "Source and target must be < number of slices!"
+#     assert s != t, "Source and target must be different!"
 
-    _, ax = plt.subplots(1, 3, figsize=(10, 5))
+#     _, ax = plt.subplots(1, 3, figsize=(10, 5))
 
-    # plot embedding of all points in gray
-    plotting.embedding(data.emb, ax=ax[0], alpha=0.05)
+#     # plot embedding of all points in gray
+#     plotting.embedding(data.emb, ax=ax[0], alpha=0.05)
 
-    # get gamma matrix for the given source-target pair
-    gammadist = data.gamma[s, t, ...]
-    np.fill_diagonal(gammadist, 0.0)
+#     # get gamma matrix for the given source-target pair
+#     gammadist = data.gamma[s, t, ...]
+#     np.fill_diagonal(gammadist, 0.0)
 
-    # color code source features
-    c = gammadist.sum(1)
-    cluster_ids = set(data.clusters["labels"][s_s])
-    labels = list(s_s)
-    for cid in cluster_ids:
-        idx = np.where(cid == data.clusters["labels"][s_s])[0]
-        for i in idx:
-            labels[i] = c[cid]
+#     # color code source features
+#     c = gammadist.sum(1)
+#     cluster_ids = set(data.clusters["labels"][s_s])
+#     labels = list(s_s)
+#     for cid in cluster_ids:
+#         idx = np.where(cid == data.clusters["labels"][s_s])[0]
+#         for i in idx:
+#             labels[i] = c[cid]
 
-    # plot source features in red
-    plotting.embedding(data.emb_2d[s_s], labels=labels, ax=ax[0], alpha=1.0)
-    prop_dict = {"style": ">", "lw": 2}
-    plotting.trajectories(data.pos[s_s], data.x[s_s], ax=ax[1], node_feature=labels, **prop_dict)
-    ax[1].set_title("Before")
+#     # plot source features in red
+#     plotting.embedding(data.emb_2d[s_s], labels=labels, ax=ax[0], alpha=1.0)
+#     prop_dict = {"style": ">", "lw": 2}
+#     plotting.trajectories(data.pos[s_s], data.x[s_s], ax=ax[1], node_feature=labels, **prop_dict)
+#     ax[1].set_title("Before")
 
-    # color code target features
-    c = gammadist.sum(0)
-    cluster_ids = set(data.clusters["labels"][s_t])
-    labels = list(s_t)
-    for cid in cluster_ids:
-        idx = np.where(cid == data.clusters["labels"][s_t])[0]
-        for i in idx:
-            labels[i] = -c[cid]  # negative for blue color
+#     # color code target features
+#     c = gammadist.sum(0)
+#     cluster_ids = set(data.clusters["labels"][s_t])
+#     labels = list(s_t)
+#     for cid in cluster_ids:
+#         idx = np.where(cid == data.clusters["labels"][s_t])[0]
+#         for i in idx:
+#             labels[i] = -c[cid]  # negative for blue color
 
-    # plot target features in blue
-    plotting.embedding(data.emb_2d[s_t], labels=labels, ax=ax[0], alpha=1.0)
-    plotting.trajectories(data.pos[s_t], data.x[s_t], ax=ax[2], node_feature=labels, **prop_dict)
-    ax[2].set_title("After")
+#     # plot target features in blue
+#     plotting.embedding(data.emb_2d[s_t], labels=labels, ax=ax[0], alpha=1.0)
+#     plotting.trajectories(data.pos[s_t], data.x[s_t], ax=ax[2], node_feature=labels, **prop_dict)
+#     ax[2].set_title("After")
