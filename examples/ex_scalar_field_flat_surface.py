7654c92ba33b4f6e0bccfdd1e01ca1aa0a99fcad
arnaudon <alexis.arnaudon@epfl.ch>
2023-04-12 17:04:33 +0200

    format script

--- examples/ex_scalar_field_flat_surface.py
+++ examples/ex_scalar_field_flat_surface.py
@@ -1,66 +1,67 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+"""This example illustrates MARBLE for a scalar field on a flat surface."""
 import sys
 from MARBLE import plotting, preprocessing, dynamics, net, postprocessing
 import matplotlib.pyplot as plt
 
-# =============================================================================
-# This example illustrates MARBLE for a scalar field on a flat surface
-# =============================================================================
+
+def f0(x):
+    return x[:, [0]] * 0
+
+
+def f1(x):
+    return x[:, [0]] + x[:, [1]]
+
+
+def f2(x):
+    return x[:, [0]] ** 2 + x[:, [1]] ** 2
+
+
+def f3(x):
+    return x[:, [0]] ** 2 - x[:, [1]] ** 2
+
 
 def main():
-    
-    #generate simple vector fields
+
+    # generate simple vector fields
     # f1: constant, f2: linear, f3: parabola, f4: saddle
     n = 512
-    x = [dynamics.sample_2d(n, [[-1,-1],[1,1]], 'random') for i in range(4)]
-    y = [f0(x[0]), f1(x[1]), f2(x[2]), f3(x[3])] #evaluated functions
-        
-    #construct PyG data object
-    data = preprocessing.construct_dataset(x, y, graph_type='cknn', k=15)
-    
-    #train model
-    params = {'epochs': 50, #optimisation epochs
-              'order': 2, #order of derivatives
-              'hidden_channels': 16, #number of internal dimensions in MLP
-              'out_channels': 5,
-              'include_self': False, #remove feature centers, for testing only, to get the figure in the SI
-              'inner_product_features': False,
-             }
-    
+    x = [dynamics.sample_2d(n, [[-1, -1], [1, 1]], "random") for i in range(4)]
+    y = [f0(x[0]), f1(x[1]), f2(x[2]), f3(x[3])]  # evaluated functions
+
+    # construct PyG data object
+    data = preprocessing.construct_dataset(x, y, graph_type="cknn", k=15)
+
+    # train model
+    params = {
+        "epochs": 50,  # optimisation epochs
+        "order": 2,  # order of derivatives
+        "hidden_channels": 16,  # number of internal dimensions in MLP
+        "out_channels": 5,
+        "include_self": False,  # remove feature centers, for testing only, to get the figure in the SI
+        "inner_product_features": False,
+    }
+
     model = net(data, params=params)
     model.run_training(data)
-    
-    #evaluate model on data
+
+    # evaluate model on data
     data = model.evaluate(data)
-    n_clusters = 10 #use 15 clusters for simple visualisation
+    n_clusters = 10  # use 15 clusters for simple visualisation
     data = postprocessing.distribution_distances(data, n_clusters=n_clusters)
-    data = postprocessing.embed_in_2D(data, embed_typ='tsne')
-    
-    #plot
-    titles=['Constant','Linear','Parabola','Saddle']
+    data = postprocessing.embed_in_2D(data, embed_typ="tsne")
+
+    # plot
+    titles = ["Constant", "Linear", "Parabola", "Saddle"]
     plotting.fields(data, titles=titles, node_size=20, col=2)
     # plt.savefig('../results/fields.svg')
     plotting.embedding(data, data.y.numpy(), titles=titles, clusters_visible=True)
-    #plt.savefig('../results/embedding.svg')
+    # plt.savefig('../results/embedding.svg')
     plotting.histograms(data, titles=titles)
     # plt.savefig('../results/histogram.svg')
     plotting.neighbourhoods(data, hops=1, norm=True, figsize=(10, 20))
     # plt.savefig('../results/neighbourhoods.svg')
-    
-def f0(x):
-    return x[:,[0]]*0
-
-def f1(x):
-    return x[:,[0]] + x[:,[1]]
-
-def f2(x):
-    return x[:,[0]]**2 + x[:,[1]]**2
-
-def f3(x):
-    return x[:,[0]]**2 - x[:,[1]]**2
+    plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
