f3210c5277ed82edc73eb17dc8a794ebe820fdb3
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2023-04-13 12:42:07 +0200

    Update helpers.py

--- examples/RNN/RNN_scripts/helpers.py
+++ examples/RNN/RNN_scripts/helpers.py
@@ -38,7 +38,7 @@ def load_network(f):
 def sample_network(net, f):
     
     if os.path.exists(f):
-        return None, torch.load(open(f, "rb"))
+        return torch.load(open(f, "rb"))
 
     n_pops = 2
     seed = 0
