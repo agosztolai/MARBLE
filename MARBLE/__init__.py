4805e864564392b46724ecb5ebc68fa87b1d4139
arnaudon <alexis.arnaudon@epfl.ch>
2023-04-14 11:20:50 +0200

    rebuilt docs

--- MARBLE/__init__.py
+++
@@ -1,5 +0,0 @@
-"""MARBLE main functions."""
-from MARBLE.main import net
-from MARBLE.postprocessing import distribution_distances
-from MARBLE.postprocessing import embed_in_2D
-from MARBLE.preprocessing import construct_dataset
