c5f57fe29270379a4d7e54ef38feb0f8e1fdb136
arnaudon <alexis.arnaudon@epfl.ch>
2023-04-14 14:09:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Setup MARBL."""
+"""Setup MARBLE."""
 import numpy
 from Cython.Build import cythonize
 from setuptools import Extension
@@ -8,6 +8,13 @@ from setuptools import setup
 setup(
     name="MARBLE",
     version="1.0",
+    author="Adam Gosztolai",
+    author_email="adam.gosztolai@epfl.ch",
+    description="""Package for the data-driven representation of non-linear dynamics
+    over manifolds based on a statistical distribution of local phase portrait features.
+    Includes specific example on dynamical systems, synthetic- and real neural datasets.""",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
     install_requires=[
         "teaspoon==1.3.1",
         "matplotlib",
