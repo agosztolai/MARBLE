bfda4255b8cb562a6bc67efd04e6035c11fd8878
Adam Gosztolai <45966708+agosztolai@users.noreply.github.com>
2024-08-19 15:42:36 +0200

    Update preprocessing.py

--- MARBLE/preprocessing.py
+++ MARBLE/preprocessing.py
@@ -189,7 +189,7 @@ def _compute_geometric_objects(
         kernels = g.gradient_op(data.pos, data.edge_index, gauges)
         Lc = None
 
-    # print("\n---- Computing eigendecomposition ... ", end="")
+    print("\n---- Computing eigendecomposition ... ", end="")
     L = g.compute_eigendecomposition(L)
     Lc = g.compute_eigendecomposition(Lc)
 
